4410a9f20baac819c07f6715e2fd2d2c69f63112
samuel <sbacon803@gmail.com>
2022-11-10 17:14:28 -0700

    add mini-max-sum prob

--- mini_max_sum.rb
+++ mini_max_sum.rb
@@ -1,21 +1,45 @@
 # given 5 positive integers
 # find minimum value possible by adding 4 of the numbers
 # find maximum value possible by adding 4 numbers
+# sort the array given to give us the min and max values in order
+# use times loop to add just the proper numbers
+# print the min value (4 lowest), and max value (4 largest) with string interpolation
 # 
 
-arr = [2, 4, 6, 9, 13]
+arr = [2, 4, 27, 9, 13]
 def min_max(arr)
-  min = 0
-  max = 0
-  min_sum = 0
-  max_sum = 0
-  sum = 0
+  sorted_array = arr.sort
+  p sorted_array
   index = 0
-  while index < arr.length
-    sum += arr[index]
+  min_sum = 0
+  while index < 4
+    min_sum = min_sum + sorted_array[index] 
     index += 1
   end
-  p index
-  p sum
+  max_sum = 0
+  while index > 0
+    max_sum = max_sum + sorted_array[index]
+    index -= 1
+  end
+  return min_sum, max_sum
 end
-min_max(arr)
+p min_max(arr)
+
+# arr = [2, 4, 27, 9, 13]
+# def min_max(arr)
+#   sorted_array = arr.sort
+#   index = 0
+#   min_sum = 0
+#   while index < 4
+#     min_sum = min_sum + sorted_array[index] 
+#     index += 1
+#   end
+#   max_sum = 0
+#   while index > 0
+#     max_sum = max_sum + sorted_array[index]
+#     index -= 1
+#   end
+#   print "#{min_sum} #{max_sum}"
+# end
+# p min_max(arr)
+
